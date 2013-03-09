6fe831535e31129c654620a7daec1f06e13e9ec4
Chris Frank <chris@thenepotist.com>
2013-03-08 19:25:31 -0500

    first commit

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+---
+---
+{% include javascripts/spin.js %}
+{% include javascripts/underscore.js %}
+{% include javascripts/backbone.js %}
+{% include javascripts/soundcloud.js %}
+{% include javascripts/vimeo.js %}
+
+$(document).ready(function(){
+  cgf.sc.init()
+  cgf.vm.init()
+})
+
